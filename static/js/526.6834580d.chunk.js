b6ad821c2631401339f280e77fc359004745ce9e
AlexeyVorobyov95 <110835875+AlexeyVorobyov95@users.noreply.github.com>
2023-04-30 22:03:48 +0000

    Deploying to gh-pages from @ AlexeyVorobyov95/goit-react-hw-08-phonebook@7aac6b7016984d0614f110b9a888847b2505ef42 🚀

---
+++ static/js/526.6834580d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[526],{4526:function(e,n,r){r.r(n),r.d(n,{default:function(){return d}});var t,o,a=r(168),c=r(225),p=c.Z.div(t||(t=(0,a.Z)(["\npadding: 55px;\n"]))),i=c.Z.h1(o||(o=(0,a.Z)(["\n  font-size: 56px;\n  color: #1c2f50;\n  padding: 265px;\n"]))),u=r(184);function d(){return(0,u.jsx)(p,{children:(0,u.jsx)(i,{children:"Phonebook manager welcome page"})})}}}]);
+//# sourceMappingURL=526.6834580d.chunk.js.map
